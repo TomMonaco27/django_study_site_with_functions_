f4a01533000134bc4622489821728e56c070e65d
TomMonaco27 <66220098+TomMonaco27@users.noreply.github.com>
2021-06-24 11:37:20 +0300

    Delete admin.py

--- baskets/7/admin.py
+++
@@ -1,9 +0,0 @@
-# Register your models here.
-from django.contrib import admin
-
-from baskets.models import Basket
-
-@admin.register(Basket)
-class BasketAdmin(admin.ModelAdmin):
-    fields = ('product', 'quantity', 'created_timestamp')
-    readonly_fields = ('created_timestamp',)
